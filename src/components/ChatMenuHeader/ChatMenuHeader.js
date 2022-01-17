503f94f4d3f13462ce1ae0fa0c56ea6177142d14
cecimermoz <cecii.mermoz@gmail.com>
2022-01-17 09:12:38 -0300

    change of logo

--- src/components/ChatMenuHeader/ChatMenuHeader.js
+++ src/components/ChatMenuHeader/ChatMenuHeader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../assets/logo.svg';
+import logo from '../../assets/loremLogo.svg';
 import { ChatMenuHeaderStyled, MenuHeaderImg } from './ChatMenuHeader.style';
 
 const ChatMenuHeader = () => {
